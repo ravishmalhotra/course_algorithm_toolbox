83713876038c85e213aaba41414224d95c2e3ccd
ravishmalhotra <ravishmalhotra.in@gmail.com>
2017-01-19 21:25:17 -0800

    first pass

--- course1/asg2/covering_segments/covering_segments.cpp
+++ course1/asg2/covering_segments/covering_segments.cpp
@@ -10,11 +10,35 @@ struct Segment {
 };
 
 vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
+  vector<int> points(1);
   //write your code here
+  //Sorting the line segments in ascending order of the first point, a(i)
+  std::sort(segments.begin(),segments.end(),[]( Segment s1 , Segment s2) {return s1.start<s2.start;});
+
+  points[0]=segments[0].start;
+  int n=0;
+
+  //Using the safe move as follows
+  //Check the first point of every segement is within bounds
+  //If it is update the current point
+  //If it is not, make a new point and update the total number of points by resizing the array.
   for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+
+   if (segments[i].start<=segments[n].end && segments[i].start>=segments[n].start){
+   points[n]=segments[i].start;
+
+   }
+
+   else {
+   n=n+1;
+   //points.resize(n);
+   //points[n]=segments[i+1].start;
+   points.push_back(segments[i].start);
+
+   }
+
+    //points.push_back(segments[i].start);
+    //points.push_back(segments[i].end);
   }
   return points;
 }
