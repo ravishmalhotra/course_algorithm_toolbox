889012789c0a7a77f2ae09344756479fff443ae5
ravishmalhotra <rmalhot@umich.edu>
2017-01-20 22:58:51 -0800

    Final accepted solution

--- course1/asg2/different_summands/different_summands.cpp
+++ course1/asg2/different_summands/different_summands.cpp
@@ -6,6 +6,35 @@ using std::vector;
 vector<int> optimal_summands(int n) {
   vector<int> summands;
   //write your code here
+  int cur_num=n;
+  int i=1;
+
+
+  //While loop that meets all the criteria
+  //Essentially, as long as the cur_num>2*(current_summand)
+  //we know, that the number wont repeat itself. If we do assume
+  //that cur_num<2*current_summand, then cur_num will be broken into
+  //summands that have already been accounted for, which will
+  // lead to repeated numbers, breaking the premise of the problem
+  //For example, 100 can be broken down 100-(1+2+....+12)=22. The number
+  //22 will be broken down into 13+9.
+  //Essentially, the next greater number i.e. 13 + a number <(22/2), a
+  // number which will always be less than 13, leading to a repeat.
+  //If you notice, this trend will continue to 104.
+  //For 105: 105-(1+2+..12)=27=13+a number >27/2 (in this case 14) and thus meets
+  // the criteria of non-repetion.
+
+
+  while ((cur_num>(2*i))) {
+
+  summands.push_back(i);
+  cur_num=cur_num-i;
+  i=i+1;
+
+  }
+
+  summands.push_back(cur_num);
+
   return summands;
 }
 
