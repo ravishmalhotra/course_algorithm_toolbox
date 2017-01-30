51866fa52b19ec1df701b417de6c9bf0667284b8
ravishmalhotra <ravishmalhotra.in@gmail.com>
2017-01-30 14:14:19 -0800

    Final commit that passes all tests

--- course1/asg3/majority_element/majority_element.cpp
+++ course1/asg3/majority_element/majority_element.cpp
@@ -4,11 +4,38 @@
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+int get_majority_element(vector<int> &a, int left, int right)
+{
+    if (left == right) return 0;
+    if (left + 1 == right) return 0;
+
+    //write your code here
+    std::sort(a.begin(), a.end());
+
+    size_t count=1;
+
+    for (size_t i=1; i<a.size(); i++)
+    {
+
+        if (a[i]==a[i-1])
+        {
+            count = count+1;
+            if (count>a.size()/2)
+                return 1;
+
+        }
+
+        else if (a[i]!=a[i-1])
+        {
+            count=1;
+        }
+
+
+    }
+
+
+
+    return 0;
 }
 
 int main() {
@@ -18,5 +45,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element(a, 0, a.size())) << '\n';
 }
