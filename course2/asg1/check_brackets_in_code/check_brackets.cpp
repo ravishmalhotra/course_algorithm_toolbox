08b25e51a4974dbe90b9f9e407b172f1b7319169
ravishmalhotra <rmalhot@umich.edu>
2017-07-09 15:52:48 -0700

    Updated comments

--- course2/asg1/check_brackets_in_code/check_brackets.cpp
+++ course2/asg1/check_brackets_in_code/check_brackets.cpp
@@ -51,7 +51,7 @@ int main()
             if (opening_brackets_stack.empty())
             {
 
-                std::cout<<position+1;
+                std::cout<<position+1; //If stack is empty, then there is an unmatched closing bracket
                 return 0;
             }
 
@@ -60,12 +60,12 @@ int main()
                 Bracket b_close(opening_brackets_stack.top());//Initialize a Bracket variable which is initialized to top of the stack
 
                 if (b_close.Matchc(next)==true)
-                    opening_brackets_stack.pop();
+                    opening_brackets_stack.pop(); //If a matching opening bracket is found, pop from stack
 
                 else
                 {
 
-                    std::cout<<position+1;
+                    std::cout<<position+1; //If no matching opening bracket is found, output the index of the unmatched closing bracket
                     return 0;
                 }
 
@@ -77,17 +77,16 @@ int main()
 
     }
 
-    if (opening_brackets_stack.empty())
+    if (opening_brackets_stack.empty()) //If all the matching brackets have been found, output Success
         std::cout<<"Success";
 
     else
     {
-        Bracket b_remaining(opening_brackets_stack.top());
+        Bracket b_remaining(opening_brackets_stack.top()); //If there are any remaining unmatched open brackets, then output the index of the first one
         std::cout<<b_remaining.position+1;
     }
 
 
-    // Printing answer, write your code here
 
     return 0;
 }
