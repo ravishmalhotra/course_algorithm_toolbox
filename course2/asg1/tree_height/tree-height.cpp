5fd39480bda243065c2d8f25e06a3a078d8f92ae
ravishmalhotra <rmalhot@umich.edu>
2017-07-14 00:09:12 -0700

    Final Solution

--- course2/asg1/tree_height/tree-height.cpp
+++ course2/asg1/tree_height/tree-height.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <stack>
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
@@ -28,27 +29,81 @@ int main_with_large_stack_space() {
   std::ios_base::sync_with_stdio(0);
   int n;
   std::cin >> n;
-
+  int root_index;
   std::vector<Node> nodes;
+
+
   nodes.resize(n);
   for (int child_index = 0; child_index < n; child_index++) {
     int parent_index;
     std::cin >> parent_index;
     if (parent_index >= 0)
       nodes[child_index].setParent(&nodes[parent_index]); //Assign parent to each number (label). For instance, index 0 gets a parent of node 4.Ex. 5  4 -1 4 1 1
+
+
+    else
+    root_index=child_index;
+
     nodes[child_index].key = child_index; //Key is the current index
   }
 
-  // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) { //Go through all the indices
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent) //For each index, continue the for loop if v!=NULL and the heirarchy of the parents of this index has been resolved
-      height++;
-    maxHeight = std::max(maxHeight, height); //The index with the largest heirarchy of parents will determine the height of the tree.
-  }
+  std::stack<Node> root;
+  root.push(nodes[root_index]);
+  int height=1;
+  int maxheight=0;
+  Node *root_current; //Using pointers because we work children pointer variable that needs to be referenced below
+  Node *parent_current;
+   std::vector <unsigned int> children_done(n);
 
-  std::cout << maxHeight << std::endl;
+    while (!(root.empty()))
+    {
+
+        root_current=&(root.top());
+
+
+
+        if (root_current->children.size()<1 || root_current->children.size()<=children_done[root_current->key]) //If we have reached the leaf node or all the children of the current have been traversed
+        {
+            root.pop(); //Pop the current node
+            maxheight=std::max(height,maxheight); //Find out the max of current height and max height
+            height--;//After poping, you are back on the parent, so height reduces by 1
+
+            if (!(root.empty()))
+            {
+
+                parent_current=&(root.top()); //Look at the parent of the child that was popped.
+
+                if (parent_current->children.size()>children_done[parent_current->key]) //If total number of children is greater than the children that have been traversed,
+                    //Push the next child to the stack and update the children that have been traversed
+                {
+
+                    //root.push(*(parent_current->children[0]));
+                    root.push(*(parent_current->children[children_done[parent_current->key]]));
+                    children_done[parent_current->key]++;
+                    height++;
+
+
+                }
+
+            }
+
+        }
+
+        else //else: children exist and they have not been traversed before
+        {
+            height++; //Increase the height
+            root.push(*(root_current->children[children_done[root_current->key]])); //Push the new children on the stack
+            children_done[root_current->key]++; //Increase the number of children that have been traversed
+
+
+
+        }
+
+    }
+
+
+
+  std::cout << maxheight << std::endl;
   return 0;
 }
 
